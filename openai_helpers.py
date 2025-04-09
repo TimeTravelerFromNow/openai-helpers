639b06bec469287b581b85b9829b9258ce7f8644
Sebastian Detering <98575184+TimeTravelerFromNow@users.noreply.github.com>
2025-04-09 14:30:13 -0700

    Handle incomplete status
    Add jupyter caches to gitignore

--- openai_helpers.py
+++ openai_helpers.py
@@ -5,6 +5,7 @@
 #######
 from dotenv import load_dotenv
 from openai import OpenAI
+import time
 
 load_dotenv()
 client = OpenAI()
@@ -22,17 +23,12 @@ def retrieve_assistant_by_id(assistant_id):
 def upload_to_file_store(file_paths):
     # Ready the files for upload to OpenAI
     file_streams = [open(path, "rb") for path in file_paths]
-    
+
     # Use the upload and poll SDK helper to upload the files, add them to the vector store,
     # and poll the status of the file batch for completion.
     file_batch = client.vector_stores.file_batches.upload_and_poll(
       vector_store_id=file_store.id, files=file_streams
     )
-
-def remove_from_file_store(file_paths):
-    #cleanup code
-    print('implement me CLEANUP remove_from_file_store!!')
-
 def get_processed_run(run, thread_id):
     MAX_ITER=100 # 30 seconds max
     SLEEP=0.3
@@ -40,13 +36,20 @@ def get_processed_run(run, thread_id):
         thread_id=thread_id,
         run_id=run.id
     )
-    while run.status == 'queued' and i < MAX_ITER:
+    i = 0
+    status = run.status
+    is_incomplete_status = (run.status == 'queued' or run.status == 'in_progress')
+    while is_incomplete_status and i < MAX_ITER:
         time.sleep(SLEEP)
         i += 1
-    if run.status == 'queued':
-        raise Exception('Assistant stuck in queued: TIMEOUT after {} seconds'.format(SLEEP*MAX_ITER))
+    if is_incomplete_status:
+        raise Exception('Assistant stuck with {} status: TIMEOUT after {} seconds'.format(run.status,SLEEP*MAX_ITER))
     else:
         return run
+
+### DELETE FUNCTIONS
+def remove_from_file_store(file_paths):
+    print('implement me CLEANUP remove_from_file_store!!')
 
 def delete_thread(thread_id=''):
     result = client.beta.threads.delete(thread_id)
