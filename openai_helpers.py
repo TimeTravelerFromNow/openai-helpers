04944fa2ec8ab140a4fbc242c5290068de7e4a74
Sebastian Detering <98575184+TimeTravelerFromNow@users.noreply.github.com>
2025-04-10 12:28:48 -0700

    Handle multiple tool_calls

--- openai_helpers.py
+++ openai_helpers.py
@@ -27,6 +27,20 @@ def upload_to_file_store(file_paths):
     file_batch = client.vector_stores.file_batches.upload_and_poll(
       vector_store_id=file_store.id, files=file_streams
     )
+    return file_batch
+
+# get_latest_message(thread_id)
+# returns content of string of latest message posted to the thread.
+def get_latest_message(thread_id):
+    thread_messages = client.beta.threads.messages.list(thread_id)
+    latest_message = thread_messages.data[0]
+    content = latest_message.content
+    for content in latest_message.content:
+        if content.type == "text":
+            return content.text.value
+        else:
+            print("Dont know how to get latest message type {}".format(content.type))
+            return ""
 
 def print_message_history(thread_id):
     messages = client.beta.threads.messages.list(thread_id)
@@ -40,16 +54,20 @@ def print_message_history(thread_id):
 
 
 def get_processed_run(run, thread_id):
+    if run.status == "expired":
+        raise Exception("Assistant run expired {}".format(run))
     MAX_ITER=100 # 30 seconds max
     SLEEP=0.3
     i = 0
     is_incomplete_status = (run.status == 'queued' or run.status == 'in_progress')
+    print("polling run {} ".format(run.id),end="")
     while is_incomplete_status and i < MAX_ITER:
-        is_incomplete_status = (run.status == 'queued' or run.status == 'in_progress')
         run = client.beta.threads.runs.retrieve(
             thread_id=thread_id,
             run_id=run.id
         )
+        is_incomplete_status = (run.status == 'queued' or run.status == 'in_progress')
+        print(".",end="")
         time.sleep(SLEEP)
         i += 1
     if is_incomplete_status:
@@ -89,24 +107,13 @@ def handle_run_result(run=None,thread_id='',_func_caller=None,is_recursing=False
             # check required action type is to submit tool outputs
             if run.required_action.type == 'submit_tool_outputs':
                 required_action = run.required_action.submit_tool_outputs
-                tool_call = required_action.tool_calls[0]
-                function_name = tool_call.function.name
-                arguments = tool_call.function.arguments
-                if tool_call.type == 'function':
-                    # function call
-                    print("Assistant is calling the {} function \n with arguments: {}".format(function_name, arguments))
-                    json_args = json.loads(arguments)
-                    tool_output = _func_caller(function_name, json_args)
-                    run = client.beta.threads.runs.submit_tool_outputs(
-                        thread_id=thread_id,
-                        run_id=run.id,
-                        tool_outputs=[
-                            {
-                                "tool_call_id":tool_call.id,
-                                "output": tool_output
-                            }
-                        ]
-                    )
+                run =serve_tool_calls(
+                    tool_calls=required_action.tool_calls,
+                    run_id=run.id,
+                    thread_id=thread_id,
+                    _func_caller=_func_caller
+                )
+
                 return handle_run_result(
                     run=run,
                     thread_id=thread_id,
@@ -117,6 +124,27 @@ def handle_run_result(run=None,thread_id='',_func_caller=None,is_recursing=False
             raise Exception('Assistant run cancelled')
         case _:
             raise Exception('Unknown assistant run status {}'.format(run.status))
+
+# handle_tool_calls
+# tool_calls: list of tool calls
+# _func_caller: function(function_name, arguments) # calls and returns custom code.
+# Define once and implement your call_custom_function somewhere in your script.
+# Returns string:
+# 'prompt_user' if the run is completed
+# 'continue_assistant' if the run is requires_action
+def serve_tool_calls(tool_calls=None, run_id="", thread_id="", _func_caller=None):
+    function_outputs = [{
+        "tool_call_id": tool_call.id,
+        "output": _func_caller(tool_call.function.name, json.loads(tool_call.function.arguments))
+    } for tool_call in tool_calls]
+
+    run = client.beta.threads.runs.submit_tool_outputs(
+                thread_id=thread_id,
+                run_id=run_id,
+                tool_outputs=function_outputs
+            )
+    return run
+
 
 ### Destructors
 def remove_from_file_store(file_paths):
